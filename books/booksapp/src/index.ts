52c087293db4501a8adb9c380c4e3ae9500236bb
João Costa Seco <jrcs@fct.unl.pt>
2023-02-27 10:56:03 +0000

    Handling a FORM

--- books/booksapp/src/index.ts
+++ books/booksapp/src/index.ts
@@ -1,11 +1,59 @@
 import http from 'http';
+import { parse } from 'querystring';
 
 const port = process.env.PORT || 8088;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+
+    switch (path) {
+        case '':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('HomePage');
+            break;
+        case '/about':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('About');
+            break;
+        case '/hello':
+            switch(req.method) {
+                case 'GET':
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`
+                    <h1>Hello, World!</h1>
+                    <form method='POST'>
+                        <input type="text" name="name" />
+                    </form>
+                    `);
+                    break;
+                case 'POST':
+                    let body = ''
+                    req.on('data', (data) => {
+                        body += data
+                    })
+                    req.on('end', () => {
+                        const name = parse(body).name
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'text/html');
+                        res.end(`<h1>Hello, ${name}!</h1>
+                        <form method='POST'>
+                            <input type="text" name="name" />
+                        </form>
+                        `);
+                    })
+                    break;
+            }
+            break;
+        default:
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Not Found');
+            break;
+    }
 });
 
 server.listen(port, () => {
