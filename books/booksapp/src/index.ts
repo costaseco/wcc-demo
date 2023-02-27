b6f26310b8b9f2d5d736ee7d15acbcd928dc530f
João Costa Seco <jrcs@fct.unl.pt>
2023-02-27 10:25:36 +0000

    Added simple server

---
+++ books/booksapp/src/index.ts
@@ -0,0 +1,13 @@
+import http from 'http';
+
+const port = process.env.PORT || 8088;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, () => {
+    console.log(`Server running at port `+port);
+});
