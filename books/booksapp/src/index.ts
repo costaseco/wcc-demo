b894668340445d96c8621b07fe01af73da0ff7ca
João Costa Seco <jrcs@fct.unl.pt>
2023-02-27 10:58:53 +0000

    Changed to Express

--- books/booksapp/src/index.ts
+++ books/booksapp/src/index.ts
@@ -1,61 +1,10 @@
-import http from 'http';
-import { parse } from 'querystring';
+import express from 'express';
 
+const app = express()
 const port = process.env.PORT || 8088;
 
-const server = http.createServer((req, res) => {
+app.use(express.static('public'))
 
-    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-
-    switch (path) {
-        case '':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('HomePage');
-            break;
-        case '/about':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('About');
-            break;
-        case '/hello':
-            switch(req.method) {
-                case 'GET':
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'text/html');
-                    res.end(`
-                    <h1>Hello, World!</h1>
-                    <form method='POST'>
-                        <input type="text" name="name" />
-                    </form>
-                    `);
-                    break;
-                case 'POST':
-                    let body = ''
-                    req.on('data', (data) => {
-                        body += data
-                    })
-                    req.on('end', () => {
-                        const name = parse(body).name
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'text/html');
-                        res.end(`<h1>Hello, ${name}!</h1>
-                        <form method='POST'>
-                            <input type="text" name="name" />
-                        </form>
-                        `);
-                    })
-                    break;
-            }
-            break;
-        default:
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('Not Found');
-            break;
-    }
-});
-
-server.listen(port, () => {
-    console.log(`Server running at port `+port);
+app.listen(port, () => {
+    console.log(`express server started at http://localhost:${port}`);
 });
